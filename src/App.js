fc7e9fe34cbbe9968ea4373b06e7d83e10659b55
XYXtobeno1 <2386662729@qq.com>
2026-04-21 16:50:04 -0400

    homepage

--- src/App.js
+++ src/App.js
@@ -1,30 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import TestComponent from "./components/TestComponent";
+import { useState } from 'react';
 
 const Comp = () => {
-  return <p>This is a comp component</p>
+  return <p>Hi, this is yunxinxu</p>
 }
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App" id='root'>
-            <TestComponent />
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button
+          onClick={() => setCount(count+1)}
+        >Clicks: {count}</button>
         <Comp />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
